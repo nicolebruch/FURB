cd8e197491af698da6e37df8a96d2f138eba3cb2
nicole <44910604+nicolebruch@users.noreply.github.com>
2026-03-08 18:01:33 -0300

    add question02

---
+++ 3° SEMESTRE/ALGORITMOS E ESTRUTURA DE DADOS/Aula 02/lista_02/src/questao_02/ListaEstatica.java
@@ -0,0 +1,117 @@
+package questao_02;
+
+public class ListaEstatica<T> {
+
+	private Object[] info;
+	private int tamanho;
+
+	public ListaEstatica() {
+		this.info = new Object[10];
+		this.tamanho = 0;
+	}
+
+	private void redimensionar() {
+		int novoTamanhoDoVetor = this.info.length + 10;
+		Object[] novoVetor = new Object[novoTamanhoDoVetor];
+
+		for (int i = 0; i < this.tamanho; i++) {
+			novoVetor[i] = this.info[i];
+		}
+
+		this.info = novoVetor;
+	}
+
+	public void inserir(T valor) {
+		if (this.tamanho == this.info.length) {
+			this.redimensionar();
+		}
+
+		int proximaPosicaoEmBranco = this.tamanho;
+		this.info[proximaPosicaoEmBranco] = valor;
+		this.tamanho++;
+	}
+
+	public void exibir() {
+		for (int i = 0; i < this.tamanho; i++) {
+			System.out.println(this.info[i]);
+		}
+	}
+
+	public int buscar(T valor) {
+		for (int i = 0; i < this.tamanho; i++) {
+			if (this.info[i] == valor) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	public void retirar(T valor) {
+		int posicao = this.buscar(valor);
+
+		if (posicao != -1) {
+			if (posicao < tamanho - 1) {
+				for (int i = posicao; i < this.tamanho - 1; i++) {
+					this.info[i] = this.info[i + 1];
+				}
+			}
+
+			this.tamanho--;
+		}
+	}
+
+	public void liberar() {
+		this.info = new Object[10];
+		this.tamanho = 0;
+	}
+
+	public T obterElemento(int posicao) {
+		try {
+			if (posicao >= this.tamanho) {
+				throw new IndexOutOfBoundsException();
+			}
+
+			return (T) this.info[posicao]; // uso cast aqui pra nao precisar ter que alterar todo o resto depois
+		} catch (IndexOutOfBoundsException ex) {
+			throw new IndexOutOfBoundsException("Elemento na posição " + posicao + " não foi encontrada");
+		}
+	}
+
+	public boolean estaVazia() {
+		return tamanho == 0;
+	}
+
+	public int getTamanho() {
+		return tamanho;
+	}
+
+	public String toString() {
+		String str = "";
+
+		for (int i = 0; i < this.tamanho; i++) {
+			str += this.info[i];
+			if (i != this.tamanho - 1) {
+				str += ",";
+			}
+		}
+
+		return str;
+	}
+
+	public void inverter() {
+		// percorre a lista só até metade, porque trocando os elementos das pontas eu
+		// nao preciso trocar o do meio
+		for (int i = 0; i < this.tamanho / 2; i++) {
+			// uso cast pq a lista é object e guardo o elemento da posicao oposta
+			T temp = (T) this.info[i];
+			// exemplo: posicao 0 recebe elemento que ta na posicao oposta
+			this.info[i] = this.info[this.tamanho - 1 - i];
+			// posicao oposta recebe o valor guardado no indice 0s
+			this.info[this.tamanho - 1 - i] = temp;
+
+		}
+
+	}
+
+}
---
+++ 3° SEMESTRE/ALGORITMOS E ESTRUTURA DE DADOS/Aula 02/lista_02/src/questao_02/ListaEstaticaTeste.java
@@ -0,0 +1,230 @@
+package questao_02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ListaEstaticaTeste {
+
+	@Test
+	@DisplayName("Testar método de inclusão de dados na lista")
+	void test01() {
+		ListaEstatica<Integer> lista = new ListaEstatica<>();
+		String esperado = "5,10,15,20";
+
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+
+		String resultado = lista.toString();
+
+		assertEquals(esperado, resultado);
+	}
+
+	@Test
+	@DisplayName("Testar método de obtenção de tamanho da lista")
+	void test02() {
+		ListaEstatica<Integer> lista = new ListaEstatica<>();
+		int tamanhoEsperado = 4;
+
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+
+		int resultado = lista.getTamanho();
+
+		assertEquals(tamanhoEsperado, resultado);
+	}
+
+	@Test
+	@DisplayName("Testar método buscar() com elemento existente")
+	void test03() {
+		ListaEstatica<Integer> lista = new ListaEstatica();
+		int buscarEsperado = 2;
+
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+
+		int resultado = lista.buscar(15);
+
+		assertEquals(buscarEsperado, resultado);
+	}
+
+	@Test
+	@DisplayName("Testar método buscar() com elemento inexistente")
+	void test04() {
+		ListaEstatica<Integer> lista = new ListaEstatica<>();
+		int buscarEsperado = -1;
+
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+
+		int resultado = lista.buscar(30);
+
+		assertEquals(buscarEsperado, resultado);
+	}
+
+	@Test
+	@DisplayName("Testar método retirar()")
+	void test05() {
+		ListaEstatica<Integer> lista = new ListaEstatica<>();
+		String esperado = "5,15,20";
+		int tamanhoEsperado = 3;
+
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+		
+		lista.retirar(10);
+
+		String resultado = lista.toString();
+		int tamanhoResultado = lista.getTamanho();
+		
+		assertEquals(esperado, resultado);
+		assertEquals(tamanhoEsperado, tamanhoResultado);
+	}
+	
+	@Test
+	@DisplayName("Testar inclusão que provoque redimensionamento")
+	void test06() {
+		ListaEstatica<Integer> lista = new ListaEstatica<>();
+		String esperado = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15";
+		int tamanhoEsperado = 15;
+		
+		lista.inserir(1);
+		lista.inserir(2);
+		lista.inserir(3);
+		lista.inserir(4);
+		lista.inserir(5);
+		lista.inserir(6);
+		lista.inserir(7);
+		lista.inserir(8);
+		lista.inserir(9);
+		lista.inserir(10);
+		lista.inserir(11);
+		lista.inserir(12);
+		lista.inserir(13);
+		lista.inserir(14);
+		lista.inserir(15);
+
+		String resultado = lista.toString();
+		int tamanhoResultado = lista.getTamanho();
+
+		assertEquals(esperado, resultado);
+		assertEquals(tamanhoEsperado, tamanhoResultado);
+	}
+	
+	@Test
+	@DisplayName("Testar método obterElemento()")
+	void test07() {
+		ListaEstatica<Integer> lista = new ListaEstatica<>();
+		int esperado = 20;
+		
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+
+		int resultado = (int) lista.obterElemento(3);
+
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	@DisplayName("Testar lançamento de exceção no método obterElemento()")
+	void test08() {
+		ListaEstatica<Integer> lista = new ListaEstatica<>();
+		
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+
+//		try {
+//			lista.obterElemento(5);
+//			fail();
+//		} catch (IndexOutOfBoundsException ex) {
+//			assertEquals("", ex.getMessage());
+//		} catch (Exception e) {
+//			fail();
+//		}
+
+		// certificar que lança: IndexOutOfBoundsException quando fazer tal coisa...
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			lista.obterElemento(5);
+		});
+	}
+	
+	@Test
+	@DisplayName("Certificar que liberar() remove todos os elementos")
+	void test09() {
+		ListaEstatica<Integer> lista = new ListaEstatica<>();
+		
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+		
+		lista.liberar();
+		
+		assertTrue(lista.estaVazia());
+	}
+
+	@Test
+	@DisplayName("Testar o método inverter() com quantidade par de dados")
+	void test10() {
+		ListaEstatica<Integer> lista = new ListaEstatica<>();
+		
+		String esperado = "20,15,10,5";
+		
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+		
+		lista.inverter();
+		
+		String resultado = lista.toString();
+		
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	@DisplayName("Testar o método inverter() com quantidade ímpar de dados")
+	void test11() {
+		ListaEstatica<Integer> lista = new ListaEstatica<>();
+		
+		String esperado = "25,20,15,10,5";
+		
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+		lista.inserir(25);
+		
+		lista.inverter();
+		
+		String resultado = lista.toString();
+		
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	@DisplayName("Testar o método inverter() com lista vazia (Extra meu mesmo)")
+	void testExtra() {
+		ListaEstatica<Integer> lista = new ListaEstatica<>();
+		
+		lista.inverter();
+		
+		assertTrue(lista.estaVazia());
+	}
+	
+}
